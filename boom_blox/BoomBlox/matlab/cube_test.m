c4a860313474b19159b7ee18e692fba45405982b
Jordan Brindza <jbrindza@gmail.com>
2012-04-11 11:51:30 -0400

    updated cube test.. still not working

--- boom_blox/BoomBlox/matlab/cube_test.m
+++ boom_blox/BoomBlox/matlab/cube_test.m
@@ -1,6 +1,9 @@
 % test script for generating sound for cube collision
 %
 
+% sample duration
+duration = 2;
+
 % sampling frequency
 fq = 44100;
 dt = 1/fq;
@@ -10,7 +13,7 @@ dt = 1/fq;
 % Young's Modulus for steel
 Y = 200; 
 % TODO: what thickness to use?
-thickness = 0.10;
+thickness = 0.01;
 
 k = Y * thickness;
 
@@ -19,13 +22,13 @@ k = Y * thickness;
 %   m = density * thickness * area
 % for now just assume homogenous object
 % density for steel is 7.85 g/cm^3
-mass = 0.785 * 3 * 0.1;
+mass = 0.785 * 3 * thickness;
 
-% fluid damping constant (must be negative)
-gamma = -1;
+% fluid damping constant (must be negative?)
+gamma = .1;
 
-% viscoelastic damping constant (must be negative)
-n_eta = -1;
+% viscoelastic damping constant (must be negative?)
+n_eta = .1;
 
 % vertices
 vert = [ -1 -1  1;  ... p111
@@ -49,30 +52,46 @@ edges = [ 2 3 5;  ...
 %}
 
 % number of vertices
-n = size(vert,1);
-
-% mass matrix
-M = mass .* eye(n);
-m = diag(M);
+%n = size(vert,1);
+n = 4;
 
 % K matrix (elastic force matrix)
 %   TODO: this is only the 1D case (should be 3nx3n)
-K = zeros(n);
-K = [ 0 1 1 0 1 0 0 0 ; ...
-      0 0 0 1 0 1 0 0 ; ...
-      0 0 0 1 0 0 1 0 ; ...
-      0 0 0 0 0 0 0 1 ; ...
-      0 0 0 0 0 1 1 0 ; ...
-      0 0 0 0 0 0 0 1 ; ...
-      0 0 0 0 0 0 0 1 ; ...
-      0 0 0 0 0 0 0 0 ];
-K = K + K';
-K = k*K;
+%K = [ 0 1 1 0 1 0 0 0 ; ...
+%      0 0 0 1 0 1 0 0 ; ...
+%      0 0 0 1 0 0 1 0 ; ...
+%      0 0 0 0 0 0 0 1 ; ...
+%      0 0 0 0 0 1 1 0 ; ...
+%      0 0 0 0 0 0 0 1 ; ...
+%      0 0 0 0 0 0 0 1 ; ...
+%      0 0 0 0 0 0 0 0 ];
+%K = K + K';
+%K = k*K;
+
+% test tetrahedral
+K = [ 0 1 1 1; ...
+      1 0 1 1; ...
+      1 1 0 1; ...
+      1 1 1 0];
+K = K + eye(n);
+%K = k*K;
+
+
+% test one triangle mesh
+%K = [ 0 1 1; ...
+%      1 0 1; ...
+%      1 1 0];
+%K = K + K';
+%K = k*K;
 
 % diagonalize K
 [G D] = eig(K);
 lambda = diag(D);
 Ginv = inv(G);
+
+% mass matrix
+M = mass .* eye(n);
+m = diag(M);
 
 % construct w vector
 %   w_i = (-(gamma*lambda_i + n) +/- sqrt((gamma*lamba_i + n)^2 - 4*lambda_i))/2
@@ -95,9 +114,6 @@ g = Ginv * f;
 % time of impact/collision
 t0 = 0;
 
-% initialize current time
-t = 0;
-
 % update constants
 c = c + g./((m .* (w_plus - w_minus)) .* exp(w_plus * t0));
 c_bar = conj(c);
@@ -107,28 +123,40 @@ c_bar = conj(c);
 %   amplitude is related to the particle vecolicty which 
 %   is linearly related to the mode velocity
 % dz/dt = c * w_plus * exp(w_plus * t) + c_bar * w_minus * exp(w_minus * t)
-vel = c .* w_plus .* exp(w_plus .* t) + c_bar .* w_minus .* exp(w_minus .* t);
+mode_vel = @(t) c .* w_plus .* exp(w_plus .* t) + c_bar .* w_minus .* exp(w_minus .* t);
+
+
+% initialize current time
+t = 0;
 
 % TODO: should there be some amplitude scaling factor? 
 %         the relation between mode vel and particle
-a = vel;
+a = mode_vel(0);
 
 
 
+nmode = length(lambda);
+nsample = fq*duration;
+mode_resp = zeros([nmode, nsample]);
+for ind = 1:fq*duration
+  % actual time
+  t = dt*ind;
+  % compute amplitude
+  v = mode_vel(t);
+  % compute sample for each mode
+  %mode_resp(:,ind) = v .* (c .* exp(w_plus*t) + c_bar .* exp(w_minus * t));
+  mode_resp(:,ind) = v .* (c .* exp(w_plus*t) + c_bar .* exp(w_minus * t));
+end
+
+% aggregate mode responses
+sample = sum(mode_resp,1);
 
 
-
-
-
-
-% for test purposes
+%% for test purposes
 c1 = c(1);
 c1_bar = c_bar(1);
 g1 = g(1);
 m1 = m(1);
 wp1 = w_plus(1);
 wm1 = w_minus(1);
-%v1 = c1 * wp1 * exp(wp1 * t) + c1_bar * wm1 * exp(wm1 * t);
-
-
-
+v1 = c1 * wp1 * exp(wp1 * t) + c1_bar * wm1 * exp(wm1 * t);
